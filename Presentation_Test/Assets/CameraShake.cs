77c3b86f8b889f4a9e8b86985060959475d07b3e
aaronwiley4208 <35614460+aaronwiley4208@users.noreply.github.com>
2018-03-15 11:45:33 -0500

    presentation stuff

---
+++ Presentation_Test/Assets/CameraShake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+	public float shakeTimer;
+	public float shakeAmount;
+	public Vector3 basePos;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (shakeTimer >= 0) {
+			Vector2 shakePos = Random.insideUnitCircle * shakeAmount;
+			transform.position = new Vector3 (transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
+			shakeTimer -= Time.deltaTime;
+
+		} 
+		else {
+			transform.position = basePos;
+		}
+	}
+
+	public void Shake(float shakePower, float shakeDur){
+		shakeTimer = shakeDur;
+		shakeAmount = shakePower;
+	}
+}
---
+++ Presentation_Test/Assets/CharController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+	//tank controls
+	//so left and right turns and up and down move forward based on where the character is facing
+
+	public float inputDelay = 0.1f;
+	public float runVelocity = 7;
+	public float rotateVelocity = 100;
+
+	public ParticleSystem spark;
+
+	//public CameraShake camscript;
+
+	Quaternion targetRotate;
+	Rigidbody rigBody;
+	float forwardInput;
+	float turnInput;
+
+	public Quaternion TargetRotation{
+		get{
+			return targetRotate;
+		}
+	}
+
+	void Start(){
+		targetRotate = transform.rotation;
+		if (GetComponent<Rigidbody>()){
+			rigBody = GetComponent<Rigidbody> ();
+		}
+	}
+
+	void Update(){
+		GetInput ();
+		Turn ();
+	}
+
+	void GetInput(){
+		forwardInput = Input.GetAxis ("Vertical");
+		turnInput = Input.GetAxis ("Horizontal");
+	}
+
+	void FixedUpdate(){
+		Run ();
+	}
+
+	void Run(){
+		if (Mathf.Abs (forwardInput) > inputDelay) {			
+			rigBody.velocity = transform.forward * forwardInput * runVelocity;
+		} else {
+			rigBody.velocity = Vector3.zero;
+		}
+	}
+
+	void Turn(){
+		targetRotate *= Quaternion.AngleAxis (rotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+		transform.rotation = targetRotate;
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "Enemy") {
+			var newspark = Instantiate (spark, col.contacts [0].point, Quaternion.identity);
+			Destroy (newspark.gameObject, 0.25f);
+			//camscript.Shake (0.25f, 0.25f);
+		}
+	}
+}
